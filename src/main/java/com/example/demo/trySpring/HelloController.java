72fc703d02bca1975aa916308301391251f054cf
tatsuya828828 <kkjy011894@gmail.com>
2020-08-01 11:32:59 +0900

    入力フォームを作成

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.trySpring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 // Springでは、コントローラークラスに@Controllerアノテーションをつける
@@ -18,5 +21,18 @@ public class HelloController {
 		// 今回の場合は、resources/templates/hello.htmlを指定している
 		// これだけで、lovalhost:8080/helloにGETリクエストが来るとhello.htmlを表示してくれる
 		return "hello";
+	}
+
+	// @PostMappingアノテーションをつけることで、POSTメソッドで送られてきた場合の処理ができるようになる
+	@PostMapping("/hello")
+	// メソッドの引数に@RequestParamアノテーションをつけることで、画面からの入力内容を受け取ることができる
+	// アノテーションの引数には、htmlのname属性の値を指定する
+	public String postRequets(@RequestParam("text1") String str1, @RequestParam("text2") String str2, Model model) {
+		// 画面から受け取った文字列をModelに登録
+		// model.addAttributeにキーと値をセットしておく、そうすることで、画面から指定したキーの値を受け取ることができる
+		model.addAttribute("name", str1);
+		model.addAttribute("like", str2);
+		// helloResponse.htmlに画面遷移
+		return "helloResponse";
 	}
 }
