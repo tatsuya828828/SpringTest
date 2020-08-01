8387b021ba5e75d4ed5b228db218d7f0a9cb4c91
tatsuya828828 <kkjy011894@gmail.com>
2020-08-01 15:44:09 +0900

    最初にINSERTするデータを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 /* ヒーローテーブルのデータ
  * このファイルに記述しているものはSpringBootを起動するたびにINSERTされる*/
-INSERT INTO hero(hero_id, hero_name, age) VALUES(1, 'アイアンマン', 49);
+INSERT INTO hero(hero_id, hero_name, name, age) VALUES(1, 'アイアンマン', 'トニー・スターク', 49);
+INSERT INTO hero(hero_id, hero_name, name, age) VALUES(2, 'キャプテン・アメリカ', 'スティーブ・ロジャース', 106)
