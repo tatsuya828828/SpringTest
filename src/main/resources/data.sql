6cc702eb43f526880e37342b86aeecee0ab0db90
tatsuya828828 <kkjy011894@gmail.com>
2020-08-06 18:34:43 +0900

    インサートするデータを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@
  * このファイルに記述しているものはSpringBootを起動するたびにINSERTされる*/
 INSERT INTO hero(hero_id, hero_name, name, age) VALUES(1, 'アイアンマン', 'トニー・スターク', 49);
 INSERT INTO hero(hero_id, hero_name, name, age) VALUES(2, 'キャプテン・アメリカ', 'スティーブ・ロジャース', 106)
+
+-- ヒーローマスターのデータ(アドミン権限)
+INSERT INTO m_hero(hero_id, password, hero_name, name, birthday, age, gender, role)
+VALUES('shield', 'shield', 'フューリー大佐', 'ニック・フューリー', '1950-07-04', 70, true, 'ROLE_ADMIN');
