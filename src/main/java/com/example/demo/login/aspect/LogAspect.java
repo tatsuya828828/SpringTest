90898cc4312794b415b781ecc82a24211351b43b
tatsuya828828 <kkjy011894@gmail.com>
2020-08-06 18:22:55 +0900

    メソッドを追加

--- src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -1,9 +1,8 @@
 package com.example.demo.login.aspect;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 // AOPのクラスには、@Aspectアノテーションをつける
@@ -20,13 +19,33 @@ public class LogAspect {
 	 * なお、パッケージ名やクラス名などには正規表現が使える
 	 * 正規表現を使うことで、コントローラークラスの全てのメソッドを指定することも簡潔に表せる
 	 */
-	@Before("execution(* *..*.*Controller.*(..))")
-	public void startLog(JoinPoint jp) {
+	@Around("execution(* *..*.*Controller.*(..))")
+	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
 		System.out.println("メソッド開始:"+ jp.getSignature());
+		try {
+			// メソッド実行
+			Object result = jp.proceed();
+			System.out.println("メソッド終了:"+ jp.getSignature());
+			return result;
+		} catch(Exception e) {
+			System.out.println("メソッド異常終了:"+ jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
-	@After("execution(* *..*.*Controller.*(..))")
-	public void endLog(JoinPoint jp) {
-		System.out.println("メソッド終了:"+ jp.getSignature());
+	// HeroDaoクラスのログ出力
+	@Around("execution(* *..*.*HeroDao*.*(..))")
+	public Object daoLog(ProceedingJoinPoint jp) throws Throwable {
+		System.out.println("メソッド開始:"+ jp.getSignature());
+		try {
+			Object result = jp.proceed();
+			System.out.println("メソッド終了:"+ jp.getSignature());
+			return result;
+		} catch(Exception e) {
+			System.out.println("メソッド異常終了:"+ jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
 	}
 }
