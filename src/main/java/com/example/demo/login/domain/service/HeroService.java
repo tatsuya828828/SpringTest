a9f0efd64ed9c62aaef164759448a9caa2c9f5bc
tatsuya828828 <kkjy011894@gmail.com>
2020-08-06 22:02:42 +0900

    insert用メソッド追加

--- src/main/java/com/example/demo/login/domain/service/HeroService.java
+++ src/main/java/com/example/demo/login/domain/service/HeroService.java
@@ -3,10 +3,26 @@ package com.example.demo.login.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.login.domain.model.Hero;
 import com.example.demo.login.domain.repository.HeroDao;
 
 @Service
 public class HeroService {
 	@Autowired
 	HeroDao dao;
+
+	// サービスクラスのinsertメソッドで、リポジトリークラスのinsertOneメソッドを呼び出している
+	// insert用メソッド
+	public boolean insert(Hero hero) {
+		// insert実行
+		int rowNumber = dao.insertOne(hero);
+		// 判定用変数
+		boolean result = false;
+		// 戻り値が0より大きければ、insertが成功したという判定結果をリターンしている
+		if(rowNumber>0) {
+			// insert成功の場合
+			result = true;
+		}
+		return result;
+	}
 }
