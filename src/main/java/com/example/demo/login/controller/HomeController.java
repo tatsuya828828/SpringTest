e83c36ae099df316cf7811bdcbe7831bcbadda20
tatsuya828828 <kkjy011894@gmail.com>
2020-08-07 14:35:06 +0900

    詳細画面取得用メソッドの追加

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.example.demo.login.controller;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.login.domain.model.Hero;
+import com.example.demo.login.domain.model.SignupForm;
 import com.example.demo.login.domain.service.HeroService;
 
 
@@ -17,6 +22,17 @@ import com.example.demo.login.domain.service.HeroService;
 public class HomeController {
 	@Autowired
 	HeroService heroService;
+
+	// 性別ステータスのラジオボタン用変数
+	private Map<String, String> radioGender;
+	// ラジオボタンの初期化メソッド
+	private Map<String, String> initRadioGender(){
+		Map<String, String> radio = new LinkedHashMap<>();
+		// 男性、女性をMapに格納
+		radio.put("男性", "true");
+		radio.put("女性", "false");
+		return radio;
+	}
 
 	// ホーム画面のGET用メソッド
 	// これは、/homeにGETリクエストがきたときに、Modelクラスの"contents"というキーに"login/home :: home_contents"という値をセットしている
@@ -32,7 +48,7 @@ public class HomeController {
 	@GetMapping("/heroList")
 	public String getHeroList(Model model) {
 		// コンテンツ部分にヒーロー一覧を表示する目の文字列を登録
-		model.addAttribute("contents", "login/hero/List :: heroList_contents");
+		model.addAttribute("contents", "login/heroList :: heroList_contents");
 		// ユーザー一覧の生成
 		List<Hero> heroList = heroService.selectMany();
 		// Modelにヒーローリストを登録
@@ -40,6 +56,39 @@ public class HomeController {
 		// データ件数を取得
 		int count = heroService.count();
 		model.addAttribute("heroListCount", count);
+		return "login/homeLayout";
+	}
+
+	// ヒーロー詳細画面のGET用メソッド
+	/* 動的なURLに対応したメソッドを作るためには、@GetMappingや@PostMappingの値に/{変数名}をつける
+	 * 例えば、ヒーローIDを受け取る場合は、@GetMapping(/heroDetail/{id})とする
+	 * 今回の場合はIDがメールアドレス形式となっているため正規表現を使用する
+	 */
+	@GetMapping("/heroDetail/{id:.+}")
+	// @PathVariableアノテーションをつけると、渡されてきたパス(URL)の値を引数の変数に入れることができる
+	public String getHeroDetail(@ModelAttribute SignupForm form, Model model, @PathVariable("id") String heroId) {
+		// ヒーローID確認(デバッグ)
+		System.out.println("heroId ="+ heroId);
+		// コンテンツ部分にユーザー詳細を表示するための文字列を登録
+		model.addAttribute("contents", "login/heroDetail :: heroDetail_contents");
+		// 性別ステータス用のラジオボタンの初期化
+		radioGender = initRadioGender();
+		// ラジオボタン用のMapをModelに登録
+		model.addAttribute("radioGender", radioGender);
+		// ヒーローIDのチェック
+		if(heroId != null && heroId.length()>0) {
+			// ヒーロー情報を取得
+			Hero hero = heroService.selectOne(heroId);
+			// Heroクラスをフォームクラスに変換
+			form.setHeroId(hero.getHeroId());
+			form.setHeroName(hero.getHeroName());
+			form.setName(hero.getName());
+			form.setBirthday(hero.getBirthday());
+			form.setAge(hero.getAge());
+			form.setGender(hero.isGender());
+			// Modelに登録
+			model.addAttribute("signupForm", form);
+		}
 		return "login/homeLayout";
 	}
 
