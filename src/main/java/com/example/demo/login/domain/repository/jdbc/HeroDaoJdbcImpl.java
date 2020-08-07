89d21f69cf2f731b6eb19670e1b605009361f46d
tatsuya828828 <kkjy011894@gmail.com>
2020-08-07 11:02:21 +0900

    select用メソッドの追加

--- src/main/java/com/example/demo/login/domain/repository/jdbc/HeroDaoJdbcImpl.java
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/HeroDaoJdbcImpl.java
@@ -1,6 +1,9 @@
 package com.example.demo.login.domain.repository.jdbc;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -10,7 +13,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.login.domain.model.Hero;
 import com.example.demo.login.domain.repository.HeroDao;
 
-// インタフェースを実装したクラス
+// インタフェースを実装したリポジトリークラス
 @Repository
 public class HeroDaoJdbcImpl implements HeroDao {
 	// JdbcTemplateはSpringが用意してくれているため、既にBean定義がされている
@@ -23,7 +26,9 @@ public class HeroDaoJdbcImpl implements HeroDao {
 	// Heroテーブルの件数を取得
 	@Override
 	public int count() throws DataAccessException {
-		return 0;
+		// 全権取得してカウント
+		int count = jdbc.queryForObject("SELECT COUNT(*) FROM m_hero", Integer.class);
+		return count;
 	}
 
 	// Heroテーブルのデータを1件取得
@@ -45,7 +50,35 @@ public class HeroDaoJdbcImpl implements HeroDao {
 	// Heroテーブルの全データを取得
 	@Override
 	public List<Hero> selectMany() throws DataAccessException {
-		return null;
+		// m_heroテーブルのデータを全権取得
+		/* カウントの結果や、カラムを1つだけ取得してくるような場合には、queryForObjectメソッドを使う
+		 * 第1引数にSQL文、第2引数に戻り値のオブジェクトのclassを指定する
+		 * 複数件selectをする場合は、queryForListメソッドを使う戻り値型には、List<Map<String, Object>>を指定する
+		 * Listが行を表していて、Mapが列を表している
+		 * Mapのgetメソッドでテーブルのカラム名を指定することで値を取得できる
+		 * 引数を追加すれば、PreparedStatementを使うこともできる
+		 * 今回の場合は、拡張for文を使って、List<Map<String, Object>>をList<Hero>に変換している
+		 */
+		List<Map<String, Object>> getList = jdbc.queryForList("SELECT * FROM m_hero");
+		// 結果返却用の変数
+		List<Hero> heroList = new ArrayList<>();
+		// 取得したデータを結果返却用のListに格納していく
+		for(Map<String, Object> map: getList) {
+			// Heroインスタンスの生成
+			Hero hero = new Hero();
+			// Heroインスタンスに取得したデータをセットする
+			hero.setHeroId((String) map.get("hero_id"));
+			hero.setPassword((String) map.get("password"));
+			hero.setHeroName((String) map.get("hero_name"));
+			hero.setName((String) map.get("name"));
+			hero.setBirthday((Date) map.get("birthday"));
+			hero.setAge((Integer) map.get("age"));
+			hero.setGender((Boolean) map.get("gender"));
+			hero.setRole((String) map.get("role"));
+			// 結果返却用のListに追加
+			heroList.add(hero);
+		}
+		return heroList;
 	}
 
 	// Heroテーブルのデータを1件更新
