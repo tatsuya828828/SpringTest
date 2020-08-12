3420f85e30742be10a44b1f779645e0409c88c59
tatsuya828828 <kkjy011894@gmail.com>
2020-08-12 10:56:20 +0900

    INSERTするデータを修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- ヒーローマスターのデータ(アドミン権限)
 INSERT INTO m_hero(hero_id, password, hero_name, name, birthday, age, gender, role)
-VALUES('shield', 'shield', 'フューリー大佐', 'ニック・フューリー', '1950-07-04', 70, true, 'ROLE_ADMIN');
+VALUES('shield@gmail.com', 'shield', 'フューリー大佐', 'ニック・フューリー', '1950-07-04', 70, true, 'ROLE_ADMIN');
