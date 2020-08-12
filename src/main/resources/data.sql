5623bbfeab41cf9ceb3d023781d40ddf083ffa2c
tatsuya828828 <kkjy011894@gmail.com>
2020-08-12 14:23:55 +0900

    INSERTするデータを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 -- ヒーローマスターのデータ(アドミン権限)
 INSERT INTO m_hero(hero_id, password, hero_name, name, birthday, age, gender, role)
 VALUES('shield@gmail.com', 'shield', 'フューリー大佐', 'ニック・フューリー', '1950-07-04', 70, true, 'ROLE_ADMIN');
+
+-- ヒーローマスターのデータ(一般ユーザー)
+INSERT INTO m_hero(hero_id, password, hero_name, name, birthday, age, gender, role)
+VALUES('ironman@gmail.com', 'ironman', 'アイアンマン', 'トニー・スターク', '1970-05-29', 50, true, 'ROLE_GENERAL');
