197c441e782cd8b9e91d15df307dd6b354f7bce8
tatsuya828828 <kkjy011894@gmail.com>
2020-08-13 18:21:46 +0900

    GETメソッド用の処理を追加

--- src/main/java/com/example/demo/login/controller/HeroRestController.java
+++ src/main/java/com/example/demo/login/controller/HeroRestController.java
@@ -1,8 +1,13 @@
 package com.example.demo.login.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.login.domain.model.Hero;
 import com.example.demo.login.domain.service.RestService;
 
 @RestController
@@ -12,4 +17,18 @@ import com.example.demo.login.domain.service.RestService;
 public class HeroRestController {
 	@Autowired
 	RestService service;
+
+	// ヒーロー全権取得
+	@GetMapping("/rest/get")
+	public List<Hero> getHeroMany(){
+		// ヒーロー全権取得
+		return service.selectMany();
+	}
+
+	// ヒーロー1件取得
+	@GetMapping("/rest/get/{id:.+}")
+	public Hero getHeroOne(@PathVariable("id") String heroId) {
+		// ユーザー1件取得
+		return service.selectOne(heroId);
+	}
 }
