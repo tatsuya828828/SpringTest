9e9141408150d8557625d88706c32cbbbdc00064
tatsuya828828 <kkjy011894@gmail.com>
2020-08-13 14:39:48 +0900

    REST用のサービスクラス作成

---
+++ src/main/java/com/example/demo/login/domain/service/RestService.java
@@ -0,0 +1,18 @@
+package com.example.demo.login.domain.service;
+
+import java.util.List;
+
+import com.example.demo.login.domain.model.Hero;
+
+public interface RestService {
+	// 1件登録用メソッド
+	public boolean insert(Hero hero);
+	// 1件検索用メソッド
+	public Hero selectOne(String heroId);
+	// 全権検索用メソッド
+	public List<Hero> selectMany();
+	// 1件更新用メソッド
+	public boolean update(Hero hero);
+	// 1件削除用メソッド
+	public boolean delete(String heroId);
+}
