9281d51ceefaeef1d957923abb96db2c975e195c
tatsuya828828 <kkjy011894@gmail.com>
2020-08-13 15:03:04 +0900

    RestMatcherクラス作成

---
+++ src/main/java/com/example/demo/RestMatcher.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+public class RestMatcher implements RequestMatcher {
+	// マッチャー
+	private AntPathRequestMatcher matcher;
+
+	// コンストラクタ
+	public RestMatcher(String url) {
+		super();
+		matcher = new AntPathRequestMatcher(url);
+	}
+
+	// URLのマッチ条件
+	@Override
+	public boolean matches(HttpServletRequest request) {
+		// GETメソッドならCSRFのチェックはしない
+		if("GET".contentEquals(request.getMethod())) {
+			return false;
+		}
+
+		// 特定のURLに該当する場合、CSRFチェックしない
+		if(matcher.matches(request)) {
+			return false;
+		}
+		return true;
+	}
+}
