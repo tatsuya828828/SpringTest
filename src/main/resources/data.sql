014d78dab761b76619b7f48ca2baf7fa99c2772d
tatsuya828828 <kkjy011894@gmail.com>
2020-08-13 11:04:48 +0900

    暗号化したパスワードをセット

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- ヒーローマスターのデータ(アドミン権限)
 INSERT INTO m_hero(hero_id, password, hero_name, name, birthday, age, gender, role)
-VALUES('shield@gmail.com', 'shield', 'フューリー大佐', 'ニック・フューリー', '1950-07-04', 70, true, 'ROLE_ADMIN');
+VALUES('shield@gmail.com', '$2a$10$csG4Y0.BiskqwGT6QgvqbuHPvfPcP4WTDx8UizqkWnPlqZgdxi4k2', 'フューリー大佐', 'ニック・フューリー', '1950-07-04', 70, true, 'ROLE_ADMIN');
 
 -- ヒーローマスターのデータ(一般ユーザー)
 INSERT INTO m_hero(hero_id, password, hero_name, name, birthday, age, gender, role)
-VALUES('ironman@gmail.com', 'ironman', 'アイアンマン', 'トニー・スターク', '1970-05-29', 50, true, 'ROLE_GENERAL');
+VALUES('ironman@gmail.com', '$2a$10$wBc.NB9D0dQfmU/Vz4Zl4OFvBIjU2ID/VBgqVAFFbcWOVasAAwDFi', 'アイアンマン', 'トニー・スターク', '1970-05-29', 50, true, 'ROLE_GENERAL');
