ae9d0084b04fb3ea3e9cd1b221754e8d580ab5fb
tatsuya828828 <kkjy011894@gmail.com>
2020-08-13 15:10:31 +0900

    RESTのみCSRFを無効に設定

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 //セキュリティ設定用クラス
 //セキュリティ設定用クラスには、@EnableWebSecurityをつける
@@ -61,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/css/**").permitAll() // cssへアクセス許可
 		.antMatchers("/login").permitAll() // ログインページは直リンクOK
 		.antMatchers("/signup").permitAll() // ヒーロー登録画面は直リンクOK
+		.antMatchers("/rest/**").permitAll()
 		// Springでは、ロール名の先頭にROLE_をつけるというルールがある
 		.antMatchers("/admin").hasAuthority("ROLE_ADMIN") // /adminは、ROLE_ADMINのヒーローのみアクセス可
 		.anyRequest().authenticated(); // それ以外は直リンク禁止
@@ -74,8 +76,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// ログアウト処理
 		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 		.logoutUrl("/logout").logoutSuccessUrl("/login");
-		// CSRF対策を無効に設定する処理
-		// http.csrf().disable();
+		// CSRF対策を無効にするURLを設定
+		RequestMatcher csrfMatcher = new RestMatcher("/rest/**");
+		// RESTのみCSRF対策を無効に設定
+		http.csrf().requireCsrfProtectionMatcher(csrfMatcher);
+
 	}
 
 	@Override
