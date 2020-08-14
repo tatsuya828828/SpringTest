901afd0e88a945a869d8e544fbaacdd9574f3ed8
tatsuya828828 <kkjy011894@gmail.com>
2020-08-14 18:36:35 +0900

    データインサートのテストメソッド追加

--- src/test/java/com/example/demo/HeroDaoTest.java
+++ src/test/java/com/example/demo/HeroDaoTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,5 +26,17 @@ public class HeroDaoTest {
 	public void countTest1() {
 		// カウントメソッドの結果が２件であることをテスト
 		assertEquals(dao.count(), 2);
+	}
+
+	// カウントメソッドのテスト2
+	@Test
+	/* @Sqlアノテーションを使用すると、そのSQLを実行した後の状態でテストされる
+	 * "!ただし、@Sqlに記載されているSQLはそのメソッド内だけで有効である
+	 * 今回の場合で言うと、countTest2のメソッド以外では、2件のデータしか返ってこないということ
+	 */
+	@Sql("/testdata.sql")
+	public void countTest2() {
+		// カウントメソッドの結果が3件であることをテスト
+		assertEquals(dao.count(), 3);
 	}
 }
